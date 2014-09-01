6557ac8dc7523e27c306e8465ac837213a69dacb
Kim To <kim.to6462@gmail.com>
2014-09-02 03:48:53 +1000

    filter enquiries with status type includes test case
    by Kim To, 02/09/2014

--- IANEW/src/uow/ia/test/hibernate/EnquiriesTest.java
+++ IANEW/src/uow/ia/test/hibernate/EnquiriesTest.java
@@ -3,6 +3,7 @@ package uow.ia.test.hibernate;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.Filter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -29,44 +30,55 @@ public class EnquiriesTest {
 	@Test
 	public void f() {
 		 List<Enquiries> enquiryList = session.createSQLQuery("SELECT * FROM Enquiries").addEntity(Enquiries.class).list();
-		 Enquiries enquiry = enquiryList.get(1);
-		 System.out.println("Enquiry ID: " + enquiry.getId());
-		 System.out.println("Contact Id: " + enquiry.getContact().getId());
-		 System.out.println("Enquiry Type: " + enquiry.getEnquiryType().getEnquiryTypeName());
-		 System.out.println("StatusTypeID " + enquiry.getStatusType().getId() + " " + enquiry.getStatusType().getStatusName());
+		 
+		 System.out.println(enquiryList.size());
+		 
+		 Filter filter = session.enableFilter("enquiryStatusFilter");
+		 filter.setParameter("enquiryStatusFilterParam", new Integer(1));
+		 
+		 List<Enquiries> enquiryList2 = session.createQuery("FROM Enquiries").list();
+		 System.out.println(enquiryList2.size());
+		 session.disableFilter("enquiryStatusFilter");
+		 
+//		 Enquiries enquiry = enquiryList.get(1);
+//		 System.out.println("Enquiry ID: " + enquiry.getId());
+//		 System.out.println("Contact Id: " + enquiry.getContact().getId());
+//		 System.out.println("Enquiry Type: " + enquiry.getEnquiryType().getEnquiryTypeName());
+//		 System.out.println("StatusTypeID " + enquiry.getStatusType().getId() + " " + enquiry.getStatusType().getStatusName());
 //		 System.out.println("ParentEnquiryId Id: " + enquiry.getParentEnquiry().getId());
-		 List<Enquiries> enquirySet = enquiry.getEnquiriesList();
-		 for(Enquiries e : enquirySet) {
-			 System.out.println("Enquiry set: " + e.getId() + " " + e.getDescription());
-		 }
-		 List<EnquiryIssues> issueTypes = enquiry.getEnquiryIssuesList();
-		 for (EnquiryIssues dt : issueTypes) {
-			 System.out.println("Issue: " + dt.getId() + " " + dt.getIssue().getIssueName()  + dt.getComment());
-		 }
+//		 List<Enquiries> enquirySet = enquiry.getEnquiriesList();
+//		 for(Enquiries e : enquirySet) {
+//			 System.out.println("Enquiry set: " + e.getId() + " " + e.getDescription());
+//		 }
+//		 List<EnquiryIssues> issueTypes = enquiry.getEnquiryIssuesList();
+//		 for (EnquiryIssues dt : issueTypes) {
+//			 System.out.println("Issue: " + dt.getId() + " " + dt.getIssue().getIssueName()  + dt.getComment());
+//		 }
+		 
 		 
 		 //update things
-		 enquiry.setDescription("Kim Test");
-		 Transaction tx = session.beginTransaction();
-		 
-		 session.saveOrUpdate(enquiry);
-		 tx.commit();
-		 
-		 tx = session.beginTransaction();
-		 enquiry.getContact().setAccommodationComment("kim test save for enquiry");
-		 enquiry.setOutOfCoverageFlag("Y");
-		 session.saveOrUpdate(enquiry.getContact());
-		 tx.commit();
-		 
-		 tx = session.beginTransaction();
-		 enquiry.getContact().setAccommodationComment("kim test save for enquiry 2");
-		 session.saveOrUpdate(enquiry.getContact());
-		 tx.commit();
-		 
-		 enquiry.setDescription("Kim Test 2");
-		 
-		 tx = session.beginTransaction();
-		 session.saveOrUpdate(enquiry.getContact());
-		 tx.commit();
+//		 enquiry.setDescription("Kim Test");
+//		 Transaction tx = session.beginTransaction();
+//		 
+//		 session.saveOrUpdate(enquiry);
+//		 tx.commit();
+//		 
+//		 tx = session.beginTransaction();
+//		 enquiry.getContact().setAccommodationComment("kim test save for enquiry");
+//		 enquiry.setOutOfCoverageFlag("Y");
+//		 session.saveOrUpdate(enquiry.getContact());
+//		 tx.commit();
+//		 
+//		 tx = session.beginTransaction();
+//		 enquiry.getContact().setAccommodationComment("kim test save for enquiry 2");
+//		 session.saveOrUpdate(enquiry.getContact());
+//		 tx.commit();
+//		 
+//		 enquiry.setDescription("Kim Test 2");
+//		 
+//		 tx = session.beginTransaction();
+//		 session.saveOrUpdate(enquiry.getContact());
+//		 tx.commit();
 	}
 	
 	@AfterMethod
